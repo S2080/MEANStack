e25199b0daf5b70e865d7a5aa80fdf83506c32f8
s2080 <gits2080@gmail.com>
2016-10-26 11:46:48 +0530

    Add dummy data

--- ContactListApp/public/controllers/controller.js
+++ ContactListApp/public/controllers/controller.js
@@ -1,6 +1,26 @@
 angular.module('myAppName',[])
-    .controller('AppCtrl', function() {
-        console.log('Hello World from Controller');
+    .controller('AppCtrl', function($scope) {
+        
+
+
+        let person1 = {
+            name : 'sujit',
+            email : 'sujitlkumbhar.gmail.com',
+            contactNumber : '8308038255'
+        }
+
+        let person2 = {
+            name : 'Ajit',
+            email : 'ajitkumbhar.gmail.com',
+            contactNumber : '9763442080'
+        }
+
+        let contactList = [];
+        contactList.push(person1);
+        contactList.push(person2);
+        $scope.contactList = contactList;
+        console.log('Hello World from Controller' + JSON.stringify(contactList));
+
     });
 
 // function AppCtrl() {
