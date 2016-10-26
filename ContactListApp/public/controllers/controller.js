2250184f76038833f6bdc0b2749ca6874763612d
s2080 <gits2080@gmail.com>
2016-10-26 18:12:23 +0530

    Update data

--- ContactListApp/public/controllers/controller.js
+++ ContactListApp/public/controllers/controller.js
@@ -1,26 +1,26 @@
+
+// var app = angular.module('myApp', []);
+// app.controller('customersCtrl', function($scope, $http) {
+//     $http.get("http://www.w3schools.com/angular/customers.php")
+//     .then(function (response) {$scope.names = response.data.records;});
+// });
+
 angular.module('myAppName',[])
-    .controller('AppCtrl', function($scope) {
+    .controller('AppCtrl', function($scope,$http) {
         
+        $http.get('/contactList').success(function(response){
+            console.log('I got the data');
+            $scope.contactList = response;
+        });
 
+       
+        // $scope.contactList = contactList;
+        // console.log('Hello World from Controller' + JSON.stringify(contactList));
 
-        let person1 = {
-            name : 'sujit',
-            email : 'sujitlkumbhar.gmail.com',
-            contactNumber : '8308038255'
-        }
-
-        let person2 = {
-            name : 'Ajit',
-            email : 'ajitkumbhar.gmail.com',
-            contactNumber : '9763442080'
-        }
-
-        let contactList = [];
-        contactList.push(person1);
-        contactList.push(person2);
-        $scope.contactList = contactList;
-        console.log('Hello World from Controller' + JSON.stringify(contactList));
-
+        // let names = [];
+        // $http.get("http://www.w3schools.com/angular/customers.php")
+        // .then(function (response) {$scope.names = response.data.records;});
+        // console.log('Hello World from Controller' + JSON.stringify(names));
     });
 
 // function AppCtrl() {
--- ContactListApp/server.js
+++ ContactListApp/server.js
@@ -12,12 +12,33 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+
+
 /**
  * get response and display into console
  */
-// app.get('/', function(request,response){
-//         response.send('Hello World from server.js');
-// });
+app.get('/contactList', function(request,response){
+       // response.send('Hello World from server.js');
+       console.log('Data is received from GET');
+        var person1 = {
+            name : 'sujit',
+            email : 'sujitlkumbhar.gmail.com',
+            contactNumber : '8308038255'
+        }
+
+        var person2 = {
+            name : 'Ajit',
+            email : 'ajitkumbhar.gmail.com',
+            contactNumber : '9763442080'
+        }
+
+        var contactList = [];
+        contactList.push(person1);
+        contactList.push(person2);
+
+
+        response.json(contactList);
+});
 
 // listen the server on port 3000
 app.listen(3000);
